09301dd40d05ea4b86dbcf739080da8e19b31673
niihara megumu <nhrmgm1223@gmail.com@>
2020-03-20 13:36:48 +0900

    mod: input_dictにpramの追加

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from app.dataaccess.google_spreadsheets import GoogleSpreadsheetsDataAccess
 from app.service.gs_command import GSCommandService
 
 input_dict = {
-    'month': '2020/03',
-    'method_mode': 'SPENDING_MONTH'
+    'month': '2020/05',
+    'method_mode': 'シート複製',
+    'copy_original': 'template',
+    'new_sheet_name': '2020/05'
 }
 
-gs_data_access = GoogleSpreadsheetsDataAccess(
-    work_field_name='HOUSE_MANAGE_FIELD')
-gs_command_service = GSCommandService(
-    gs_data_access=gs_data_access)
-gs_command_service.run(input_dict=input_dict)
+gs_data_access = GoogleSpreadsheetsDataAccess(work_field_name='HOUSE_MANAGE_FIELD')
+gs_command_service = GSCommandService(gs_data_access=gs_data_access)
 
+gs_command_service.run(input_dict=input_dict)
