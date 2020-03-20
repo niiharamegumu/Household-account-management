9f6e91baf226e557d49e7e791cb7898ed62d8d4a
niihara megumu <nhrmgm1223@gmail.com@>
2020-03-20 12:30:56 +0900

    mod:dataaccessの修正

--- app/dataaccess/google_spreadsheets.py
+++ app/dataaccess/google_spreadsheets.py
@@ -1,19 +1,28 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from app.utils.settings import EnvironmentVar
+from gspread.models import Spreadsheet
 
 
-class GoogleSpreadsheets:
-    def __init__(self, environment_var: EnvironmentVar):
-        self.environment_var = environment_var
+class GoogleSpreadsheetsDataAccess:
+    def __init__(self, work_field_name: str):
+        self.work_field_name = work_field_name
         self.scope = [
             'https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive'
         ]
         self.credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            self.environment_var.get_json_key_path(), self.scope)
+            EnvironmentVar().get_json_key_path(), self.scope)
         self.gc = gspread.authorize(self.credentials)
+        self.spreadsheet: Spreadsheet = self.open_spreadsheet()
 
-    def get_spreadsheets_key_by_work_field_name(
-            self, work_field_name: str):
-        return self.environment_var.get_environment_var_value(work_field_name)
+    def open_spreadsheet(self):
+        return self.gc.open_by_key(
+            EnvironmentVar().get_environment_var_value(self.work_field_name)
+        )
+
+    def get_range_value(self, sheet: str, start_cell: str, end_cell: str) -> list:
+        return self.spreadsheet.worksheet(sheet).range(f'{start_cell}:{end_cell}')
+
+    def get_cell_by_label(self, sheet: str, label: str):
+        return self.spreadsheet.worksheet(sheet).acell(label)
